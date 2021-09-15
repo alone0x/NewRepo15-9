055c7abbf62dccd5a11f836e94c1a974e337bb9a
thanhbt <buitrungthanh260897@gmail.com>
2021-09-15 11:34:57 +0700

    complete delete

--- Kingfisher/ViewController.swift
+++ Kingfisher/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageView
+        let url = URL(string: "https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png")
+        imageView.kf.setImage(with: url)
         // Do any additional setup after loading the view.
     }
 
