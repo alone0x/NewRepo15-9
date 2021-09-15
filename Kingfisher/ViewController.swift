b83acafb2c4135cb118de9e13422df8f28973209
thanhbt <buitrungthanh260897@gmail.com>
2021-09-15 09:32:46 +0700

    add folder

---
+++ Kingfisher/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Kingfisher
+//
+//  Created by thanhbt on 13/09/2021.
+//
+
+import UIKit
+import Kingfisher
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
